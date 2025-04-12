d7062fa1cc38b5794fdcae10e71eed2d1b63d63a
0-9-10-12 <86973222+0-9-10-12@users.noreply.github.com>
2025-04-12 16:24:22 +0900

    add proj

---
+++ Source/NetProj/NetProjGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NetProjGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANetProjGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANetProjGameMode();
+};
+
+
+
---
+++ Source/NetProj/Private/NetActor.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "NetProj/Public/NetActor.h"
+#include "Dom/JsonObject.h"
+#include "Serialization/JsonReader.h"
+#include "Serialization/JsonSerializer.h"
+
+
+// Sets default values
+ANetActor::ANetActor()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void ANetActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ANetActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
